761df3ebcaa4b6dc1855d7d7ee8961046803b064
matiasch9 <matias.ch9@gmail.com>
2022-10-19 04:15:16 -0300

    procfile editado 3

--- entregaFinal/settings.py
+++ entregaFinal/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,9 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok= True)
 os.makedirs(STATIC_ROOT, exist_ok= True)
